a55a73a018cecadc153e60d7c7dce9f9e6b6b02e
Terekhin <blackrat72@qip.ru>
2023-11-02 15:12:34 +0300

    add to task1 basic finctional

--- src/hw1/Calc/CalculatorTest.java
+++ src/hw1/Calc/CalculatorTest.java
@@ -1,4 +1,4 @@
-package Calc;
+package hw1.Calc;
 import static org.assertj.core.api.Assertions.*;
 
 public class CalculatorTest {
@@ -25,6 +25,24 @@ public class CalculatorTest {
                 .isInstanceOf(ArithmeticException.class)
                 .hasMessage("Сумма скидки не может быть больше или равна 100%");
 
-        System.out.println("All tests passed");
+
+
+
+
+        // Проверка базового функционала с целыми числами, с использованием утверждений AssertJ:
+        assertThat(Calculator.calculation(2, 6, '+')).isEqualTo(8);
+        assertThat(Calculator.calculation(7, 2, '-')).isEqualTo(5);
+        assertThat(Calculator.calculation(2, 5, '*')).isEqualTo(10);
+        assertThat(Calculator.calculation(100, 25, '/')).isEqualTo(4);
+
+        // Проверка ожидаемого исключения, с использованием утверждений AssertJ:
+        assertThatThrownBy(() ->
+                Calculator.calculation(8, 4, '_')
+                ).isInstanceOf(IllegalStateException.class);
+
+            System.out.println(Calculator.calculation(2_147_483_647, 1, '+')); // integer overflow
+            System.out.println(Calculator.squareRootExtraction(169));
+
+            System.out.println("All tests passed");
     }
 }
